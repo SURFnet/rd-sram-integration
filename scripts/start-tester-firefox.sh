6593f217afda572782f706af94a918f0e7ee3764
Michiel de Jong <michiel@mesh.pondersource.com>
2022-10-24 13:41:00 +0000

    FederatedGroupSharingManagerFactory.php

--- scripts/start-tester-firefox.sh
+++ scripts/start-tester-firefox.sh
@@ -1,2 +1,2 @@
 docker run -d --name=firefox -p 5800:5800 -v /tmp/shm:/config:rw --network=testnet --shm-size 2g jlesage/firefox:v1.17.1
-echo Now browse to http://<host>:5800 to see a Firefox instance that sits inside the Docker testnet.
+echo Now browse to http://\<host\>:5800 to see a Firefox instance that sits inside the Docker testnet.
