713d933264e3773348803f64b92192ef76d59ce4
Michiel de Jong <michiel@unhosted.org>
2022-10-24 14:47:33 +0200

    Generate certs inside apache-php

--- scripts/gencerts.sh
+++ scripts/gencerts.sh
@@ -1,42 +1,33 @@
 #!/bin/bash
 set -e
+export TLS_DIR=./servers/apache-php/tls
 
-mkdir -p tls
 function createCert {
   echo Generating key and CSR for $1.docker
   openssl req -new -nodes \
-    -out ./tls/$1.csr \
-    -keyout ./tls/$1.key \
+    -out $TLS_DIR/$1.csr \
+    -keyout $TLS_DIR/$1.key \
     -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=$1.docker"
   echo Creating extfile
-  echo "subjectAltName = @alt_names" > ./tls/$1.cnf
-  echo "[alt_names]" >> ./tls/$1.cnf
-  echo "DNS.1 = $1.docker" >> ./tls/$1.cnf
+  echo "subjectAltName = @alt_names" > $TLS_DIR/$1.cnf
+  echo "[alt_names]" >> $TLS_DIR/$1.cnf
+  echo "DNS.1 = $1.docker" >> $TLS_DIR/$1.cnf
 
   echo Signing CSR for $1.docker, creating cert.
-  openssl x509 -req -days 365 -in ./tls/$1.csr \
-    -CA ./tls/ocm-ca.crt -CAkey ./tls/ocm-ca.key -CAcreateserial \
-    -out ./tls/$1.crt -extfile ./tls/$1.cnf
+  openssl x509 -req -days 365 -in $TLS_DIR/$1.csr \
+    -CA $TLS_DIR/ocm-ca.crt -CAkey $TLS_DIR/ocm-ca.key -CAcreateserial \
+    -out $TLS_DIR/$1.crt -extfile $TLS_DIR/$1.cnf
 }
 
+echo Creating $TLS_DIR
+mkdir -p $TLS_DIR
 echo Generating CA key
-openssl genrsa -out ./tls/ocm-ca.key 2058
+openssl genrsa -out $TLS_DIR/ocm-ca.key 2058
 echo Generate CA self-signed certificate
 openssl req -new -x509 -days 365 \
-    -key ./tls/ocm-ca.key \
-    -out ./tls/ocm-ca.crt \
+    -key $TLS_DIR/ocm-ca.key \
+    -out $TLS_DIR/ocm-ca.crt \
     -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=ocm-ca"
 
-createCert nc1
-createCert nc2
 createCert oc1
 createCert oc2
-createCert stub1
-createCert stub2
-createCert revad1
-createCert revad2
-createCert revanc1
-createCert revanc2
-createCert revaoc1
-createCert revaoc2
-createCert meshdir
--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,42 +1,10 @@
 #!/bin/bash
 set -e
 
-# docker build -t tester .
-
-# image for stub1 and stub2:
-cd ocm-stub
-cp -r ../tls .
-docker build -t stub .
-cd ..
-
-# image for running the tests from Github Actions:
-cd servers/ci
-cp -r ../../tls .
-docker build -t ci .
-
-# image for revad1, revad2, revanc1, revanc2:
-cd ../revad
-cp -r ../../tls .
-# docker build -t revad --build-arg CACHEBUST=`date +%s` .
-docker build -t revad .
-
-# base image for nextcloud image and owncloud image:
-cd ../apache-php
+# base image for owncloud image:
+cd servers/apache-php
 cp -r ../../tls .
 docker build -t apache-php .
-
-# base image for nc1 image and nc2 image:
-cd ../nextcloud
-# docker build -t nextcloud --build-arg CACHEBUST=`date +%s` .
-docker build -t nextcloud .
-
-# image for nc1:
-cd ../nc1
-docker build -t nc1 .
-
-# image for nc2:
-cd ../nc2
-docker build -t nc2 .
 
 # base image for oc1 image and oc2 image:
 cd ../owncloud
