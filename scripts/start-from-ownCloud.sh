c3c8d1c8cdc60b76372d7ce1f6c34d55115b2e7a
Michiel de Jong <michielbdejong@users.noreply.github.com>
2022-10-24 16:48:08 +0200

    Federated group sharing manager factory, fix #10 (#11)
    * FederatedGroupSharingManagerFactory.php
    
    * load FederatedGroupSharingManagerFactory
    
    * update class comment
    
    * FederatedGroupsSharingManagerFactory
    
    * FederatedGroupsProviderFactory
    
    * Use SurfShareProvider as the default one
    
    * SurfShareProvider
    
    * OC\Share20\Surf\SurfShareProvider;
    
    * Start work on sendOcmInvites
    
    * Add FIXME note for #12
    
    Co-authored-by: Michiel de Jong <michiel@mesh.pondersource.com>

--- scripts/start-from-ownCloud.sh
+++ scripts/start-from-ownCloud.sh
@@ -4,7 +4,7 @@ echo "starting with start-from-ownCloud.sh"
 echo "starting maria1.docker"
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 echo "starting oc1.docker"
-docker run -d --network=testnet --name=oc1.docker -v /root/oc-sciencemesh:/var/www/html/apps/sciencemesh oc1
+docker run -d --network=testnet --name=oc1.docker -v /root/rd-sram-integration/Surf:/var/www/html/lib/private/Share20/Surf oc1
 echo "sleeping"
 sleep 15
 echo "executing init.sh on oc1.docker"
