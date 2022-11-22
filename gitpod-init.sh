2dddaeab8d1be6ef7245e5384d7a92ada317dc2b
Michiel de Jong <michiel@unhosted.org>
2022-11-22 10:45:35 +0000

    Pull docker and core

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -4,4 +4,6 @@ set -e
 git clone --branch=accept-ocm-to-groups --depth=1 http://github.com/pondersource/core
 ./scripts/gencerts.sh
 ./scripts/rebuild.sh
+docker pull mariadb
+docker pull jlesage/firefox:v1.17.1
 docker network create testnet
