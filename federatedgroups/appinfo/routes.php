a0707e0d0aa9a4b144ad82d1a5c82b1d1fc52c16
root <root@cloud.pondersource.com>
2023-03-22 16:01:04 +0000

    RD-API controller

--- federatedgroups/appinfo/routes.php
+++ federatedgroups/appinfo/routes.php
@@ -8,19 +8,27 @@
  *
  */
 
-return [
-	'routes' => [
-		// Groups
-		['name' => 'scim#getGroups', 'url' => '/scim/Groups', 'verb' => 'GET'],
-		['name' => 'scim#createGroup', 'url' => '/scim/Groups', 'verb' => 'POST'],
-		['name' => 'scim#updateGroup', 'url' => '/scim/Groups', 'verb' => 'PUT'],
-		
-		['name' => 'scim#addUserToGroup', 'url' => '/scim/Groups/{groupId}', 'verb' => 'PATCH'],
+ $ROLE = "RD-API";
 
-		// Users
-		['name' => 'scim#getUsers', 'url' => '/scim/Users', 'verb' => 'GET'],
-		['name' => 'scim#createUser', 'url' => '/scim/Users', 'verb' => 'POST'],
-		['name' => 'scim#updateUser', 'url' => '/scim/Users', 'verb' => 'PUT'],
-	],
-	'resources' => []
-];
+if ($ROLE == "RD-API") {
+	return [
+		'routes' => [
+			// Groups
+			['name' => 'rdapi#getGroups', 'url' => '/scim/Groups', 'verb' => 'GET'],
+			['name' => 'rdapi#createGroup', 'url' => '/scim/Groups', 'verb' => 'POST'],
+			['name' => 'rdapi#updateGroup', 'url' => '/scim/Groups', 'verb' => 'PUT'],
+			// Users
+			['name' => 'rdapi#getUsers', 'url' => '/scim/Users', 'verb' => 'GET'],
+			['name' => 'rdapi#createUser', 'url' => '/scim/Users', 'verb' => 'POST'],
+			['name' => 'rdapi#updateUser', 'url' => '/scim/Users', 'verb' => 'PUT'],
+		],
+		'resources' => []
+	];
+} else {
+	return [
+		'routes' => [
+			['name' => 'scim#createGroup', 'url' => '/scim/Groups', 'verb' => 'POST'],
+		],
+		'resources' => []
+	];
+}
---
+++ federatedgroups/lib/Controller/RdapiController.php
@@ -0,0 +1,179 @@
+<?php
+
+/**
+ * @author Michiel de Jong <michiel@pondersource.com>
+ *
+ * @copyright Copyright (c) 2023, SURF
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\FederatedGroups\Controller;
+
+use Exception;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\JSONResponse;
+use OCP\IRequest;
+
+const DEMO_DOMAIN = "pondersource.net";
+
+/**
+ * Class RdapiController
+ *
+ * @package OCA\FederatedGroups\Controller
+ */
+class RdapiController extends Controller {
+	private $users;
+	private $groups;
+
+	/**
+	 * OcmController constructor.
+	 *
+	 * @param string $appName
+	 * @param IRequest $request
+	 */
+	public function __construct(
+		$appName,
+		IRequest $request
+	) {
+		parent::__construct($appName, $request);
+		$stored = file_get_contents('./users.json');
+		$this->users = json_decode($stored, true);
+
+		$stored = file_get_contents('./groups.json');
+		$this->groups = json_decode($stored, true);
+	}
+
+	private function saveUsers() {
+		error_log("Saving users.json");
+		file_put_contents('./users.json', json_encode($this->users));
+	}
+	
+	private function saveGroups() {
+		error_log("Saving groups.json");
+		file_put_contents('./groups.json', json_encode($this->groups));
+	}
+
+	private function lookupUser($user) {
+		$emailAddress = $user["emails"][0]["value"];
+		error_log(var_export($user["emails"], true));
+		error_log("Email address is $emailAddress");
+		$splitAtSign = explode("@", $emailAddress);
+		$splitPlusSign = explode("+", $splitAtSign[0]);
+		if (count($splitPlusSign) == 2) {
+			error_log("user is recognized: " . $splitPlusSign[0] . "@" . $splitPlusSign[1] . "." . DEMO_DOMAIN);
+			return $splitPlusSign[0] . "@" . $splitPlusSign[1] . "." . DEMO_DOMAIN;
+		}
+		error_log("user is not recognized: " . $user["externalId"]);
+		return $user["externalId"];
+	}
+
+	private function lookupGroup($group) {
+		return $group["displayName"];
+	}
+
+	/**
+	 * @NoCSRFRequired
+	 * @PublicPage
+	 */
+	public function getUsers() {
+		error_log("scim get users ");
+		$filter = $this->request->getParam('filter', null); // externalId eq "1dad78c9-c74b-4f7d-9f98-eab912cbfd07@sram.surf.nl"
+		$qs = explode(" ", $filter);
+		list($field, $condition, $value) = $qs; // [$field, $condition, $value] = $qs;
+		$id = json_decode($value); // strip quotes
+		if (isset($this->users[$id])) {
+			error_log("User $value exists!");
+			return new JSONResponse([
+				"totalResults" => 1,
+				"Resources" => [
+					$this->users[$id],
+				],
+			], Http::STATUS_OK);
+		} else {
+			error_log("User $value exists not!");
+			return new JSONResponse([], Http::STATUS_NOT_FOUND);
+		}
+	}
+
+
+	/**
+	 * @NoCSRFRequired
+	 * @PublicPage
+	 */
+	public function createUser() {
+		error_log("scim create user");
+		$body = file_get_contents("php://input");
+		$obj = json_decode($body, true);
+
+		error_log("=========================bodyJson=============================");
+		error_log(var_export($obj, true));
+		error_log("=========================bodyJson=============================");
+		$obj["id"] = $this->lookupUser($obj);
+    $this->users[$obj["externalId"]] = $obj;
+		error_log("User added, saving!");
+		$this->saveUsers();
+		return new JSONResponse(
+			$obj,
+			Http::STATUS_CREATED
+		);
+	}
+
+	/**
+	 * @NoCSRFRequired
+	 * @PublicPage
+	 */
+	public function getGroups() {
+		error_log("scim get groups");
+		$filter = $this->request->getParam('filter', null); // externalId eq "1dad78c9-c74b-4f7d-9f98-eab912cbfd07@sram.surf.nl"
+		$qs = explode(" ", $filter);
+		list($field, $condition, $value) = $qs; // [$field, $condition, $value] = $qs;
+		$id = json_decode($value); // strip quotes
+		if (isset($this->groups[$id])) {
+			error_log("Group $value exists!");
+			return new JSONResponse([
+				"totalResults" => 1,
+				"Resources" => [
+					$this->groups[$id],
+				],
+			], Http::STATUS_OK);
+		} else {
+			error_log("Group $value exists not!");
+			return new JSONResponse([], Http::STATUS_NOT_FOUND);
+		}
+	}
+
+	/**
+	 * @NoCSRFRequired
+	 * @PublicPage
+	 */
+	public function createGroup() {
+		error_log("scim create group");
+		$obj = json_decode(file_get_contents("php://input"), true);
+
+		error_log("=========================bodyJson=============================");
+		error_log(var_export($obj, true));
+		error_log("=========================bodyJson=============================");
+		$obj["id"] = $this->lookupGroup($obj);
+    $this->groups[$obj["externalId"]] = $obj;
+		$this->saveGroups();
+		return new JSONResponse(
+			$obj,
+			Http::STATUS_CREATED
+		);
+	}
+
+}
