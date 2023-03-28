522077d4b1080e9c0243444e0fadd4e68a3bf3a7
root <root@almere.pondersource.net>
2023-03-28 09:48:00 +0000

    watch more tables; :

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,1 +1,3 @@
 docker exec -it maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "SELECT * FROM oc_group_user;"
+docker exec -it maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "SELECT * FROM oc_share;"
+docker exec -it maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "SELECT * FROM oc_share_external_group;"
