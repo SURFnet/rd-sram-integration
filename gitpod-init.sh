ef8503554a282f20e81ae2a300753a84e7b6fd59
root <root@cloud.pondersource.com>
2023-03-31 14:48:43 +0000

    ...

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-git clone --branch=surf-dev --depth=1 https://github.com/pondersource/core
-# if [ ! -d "./core" ];
-# then 
-# 	echo -e "It's not there\n"
-#     git clone --branch=surf-dev --depth=1 https://github.com/pondersource/core
-# fi
+# git clone --branch=surf-dev --depth=1 https://github.com/pondersource/core
+if [ ! -d "./core" ];
+then 
+	echo -e "It's not there\n"
+    git clone --branch=surf-dev --depth=1 https://github.com/pondersource/core
+fi
 ./scripts/gencerts.sh
 ./scripts/rebuild.sh
 docker pull mariadb
