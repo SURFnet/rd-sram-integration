bbda90994f7b0ebcef26b317be8ab81b04ab1784
Michiel de Jong <michiel@unhosted.org>
2023-04-14 13:39:14 +0000

    work around https://github.com/SURFnet/rd-sram-integration/issues/147

--- opencloudmesh/lib/ShareeSearchPlugin.php
+++ opencloudmesh/lib/ShareeSearchPlugin.php
@@ -183,14 +183,14 @@ class ShareeSearchPlugin implements IRemoteShareesSearch {
 				|| !$this->isInstanceDomain($search))
 		) {
 			$this->result['exact']['remotes'][] = [
-				'label' => $search,
+				'label' => "U:$search",
 				'value' => [
 					'shareType' => Share::SHARE_TYPE_REMOTE,
 					'shareWith' => $search,
 				],
 			];
 			$this->result['exact']['remotes'][] = [
-				'label' =>  $search,
+				'label' => "G:$search",
 				'value' => [
 					'shareType' => Share::SHARE_TYPE_REMOTE_GROUP,
 					'shareWith' => $search,
