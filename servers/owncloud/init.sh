9506e5c5ff8e3867b5c125df73f3935d135f0aea
Reza Soltani <soltanireza65@gmail.com>
2023-05-01 10:04:54 +0000

    ...

--- servers/owncloud/init.sh
+++ servers/owncloud/init.sh
@@ -8,6 +8,6 @@ echo Editing Config
 # sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
 sed -i "8 i\      1 => 'oc1.docker'," /var/www/html/config/config.php
 sed -i "9 i\      2 => 'oc2.docker'," /var/www/html/config/config.php
-sed -i "10 i\     3 =>  $_SERVER['HTTP_HOST'],," /var/www/html/config/config.php
+sed -i "9 i\      2 => $_SERVER['HTTP_HOST']," /var/www/html/config/config.php
 sed -i "3 i\  'sharing.managerFactory' => 'OCA\\\\FederatedGroups\\\\ShareProviderFactory'," /var/www/html/config/config.php
 sed -i "4 i\  'sharing.remoteShareesSearch' => 'OCA\\\\FederatedGroups\\\\ShareeSearchPlugin'," /var/www/html/config/config.php
