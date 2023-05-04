e497640e693e0265ec68cdda63f4e9512ac87dd5
Navid Shokri <navid.pdp11@gmail.com>
2023-05-04 19:03:14 +0000

    add Sram share provider

--- federatedgroups/lib/AppInfo/Application.php
+++ federatedgroups/lib/AppInfo/Application.php
@@ -20,6 +20,13 @@ use OCP\Share\Events\DeclineShare;
 use OCP\Util;
 use OCP\IContainer;
 use OCA\FederatedFileSharing\TokenHandler;
+use OCA\FederatedFileSharing\AddressHandler;
+use OCA\FederatedFileSharing\DiscoveryManager;
+use OCA\FederatedFileSharing\Ocm\NotificationManager;
+use OCA\OpenCloudMesh\FederatedFileSharing\GroupNotifications;
+use OCA\FederatedFileSharing\Ocm\Permissions;
+use OCA\FederatedGroups\SRAMFederatedGroupShareProvider;
+
 
 class Application extends App {
 	private $isProviderRegistered = false;
@@ -32,7 +39,7 @@ class Application extends App {
   	public static  function getMixedGroupShareProvider() {
 		$urlGenerator = \OC::$server->getURLGenerator();
 		$l10n = \OC::$server->getL10N('federatedfilesharing');
-		$addressHandler = new \OCA\FederatedFileSharing\AddressHandler(
+		$addressHandler = new AddressHandler(
 			$urlGenerator,
 			$l10n
 		);
@@ -52,6 +59,51 @@ class Application extends App {
 			$addressHandler,
 			$l10n,
 			\OC::$server->getLogger()
+		);
+	}
+
+	public function getSRAMFederatedGroupShareProvider(){
+		
+		
+		$addressHandler = new AddressHandler(
+			\OC::$server->getURLGenerator(),
+			\OC::$server->getL10N('federatedfilesharing')
+		);
+		$discoveryManager = new DiscoveryManager(
+			\OC::$server->getMemCacheFactory(),
+			\OC::$server->getHTTPClientService()
+		);
+		$notificationManager = new NotificationManager(
+			new Permissions()
+		);
+		$notifications = new GroupNotifications(
+			$addressHandler,
+			\OC::$server->getHTTPClientService(),
+			$discoveryManager,
+			$notificationManager,
+			\OC::$server->getJobList(),
+			\OC::$server->getConfig()
+		);
+		$tokenHandler = new TokenHandler(
+			\OC::$server->getSecureRandom()
+		);
+
+		return new SRAMFederatedGroupShareProvider(
+			\OC::$server->getDatabaseConnection(),
+			\OC::$server->getEventDispatcher(),
+			$addressHandler,
+			$notifications,
+			$tokenHandler,
+			\OC::$server->getL10N('federatedfilesharing'),
+			\OC::$server->getLogger(),
+			\OC::$server->getLazyRootFolder(),
+			\OC::$server->getConfig(),
+			\OC::$server->getUserManager(),
+			$this->getContainer()->query('OCA\\FederatedGroups\\ShareProviderFactory'),
+			function () {
+				$ocmApp = new \OCA\OpenCloudMesh\AppInfo\Application();
+				return $ocmApp->getContainer()->query('OCA\\OpenCloudMesh\\GroupExternalManager');
+			}
 		);
 	}
 } 
---
+++ federatedgroups/lib/SRAMFederatedGroupShareProvider.php
@@ -0,0 +1,151 @@
+<?php
+
+// Copyright (c) 2018, ownCloud GmbH
+// SPDX-FileCopyrightText: 2022 SURF
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\FederatedGroups;
+
+use OC\Share20\Exception\BackendError;
+use OC\Share20\Exception\InvalidShare;
+use OC\Share20\Exception\ProviderException;
+use OC\Share20\Share;
+use OC\Share20\DefaultShareProvider;
+use OCA\FederatedFileSharing\Address;
+use OCA\FederatedFileSharing\AddressHandler;
+use OCA\OpenCloudMesh\FederatedGroupShareProvider;
+use OCA\FederatedFileSharing\DiscoveryManager;
+use OCA\FederatedFileSharing\Ocm\NotificationManager;
+use OCA\FederatedFileSharing\Ocm\Permissions;
+use OCA\FederatedFileSharing\TokenHandler;
+use OCA\OpenCloudMesh\FederatedFileSharing\GroupNotifications;
+use OCA\OpenCloudMesh\Files_Sharing\External\Manager;
+use OCP\Files\File;
+use OCP\Share\IAttributes;
+use OCP\Share\IProviderFactory;
+use OCP\Share\IShare;
+use OCP\Share\IShareProvider;
+use OCP\Share\Exceptions\ShareNotFound;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\Files\IRootFolder;
+use OCP\Files\Node;
+use OCP\IConfig;
+use OCP\IGroup;
+use OCP\IL10N;
+use OCP\ILogger;
+use OCP\IUserManager;
+use OCP\Share\Constants;
+use OCP\IDBConnection;
+use OCA\FederatedFileSharing;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Class SRAMFederatedGroupShareProvider
+ *
+ * @package OCA\FederatedGroups
+ */
+class SRAMFederatedGroupShareProvider extends FederatedGroupShareProvider {
+
+	/** @var IDBConnection */
+	private $dbConnection;
+
+	/**
+	 * FederatedGroupShareProvider constructor.
+	 *
+	 * @param IDBConnection $connection
+	 * @param EventDispatcherInterface $eventDispatcher
+	 * @param AddressHandler $addressHandler
+	 * @param GroupNotifications $notifications
+	 * @param TokenHandler $tokenHandler
+	 * @param IL10N $l10n
+	 * @param ILogger $logger
+	 * @param IRootFolder $rootFolder
+	 * @param IConfig $config
+	 * @param IUserManager $userManager
+	 * @param IProviderFactory $shareProviderFactory
+	 * @param callable $externalManagerProvider
+	 */
+	public function __construct(
+		IDBConnection $connection,
+		EventDispatcherInterface $eventDispatcher,
+		AddressHandler $addressHandler,
+		GroupNotifications $notifications,
+		TokenHandler $tokenHandler,
+		IL10N $l10n,
+		ILogger $logger,
+		IRootFolder $rootFolder,
+		IConfig $config,
+		IUserManager $userManager,
+		IProviderFactory $shareProviderFactory,
+		callable $externalManagerProvider
+	) {
+		parent::__construct(
+			$connection,
+			$eventDispatcher,
+			$addressHandler,
+			$notifications,
+			$tokenHandler,
+			$l10n,
+			$logger,
+			$rootFolder,
+			$config,
+			$userManager,
+			$shareProviderFactory,
+			$externalManagerProvider
+		);
+		$this->dbConnection = $connection;
+	}
+
+	/**
+	 * Return the identifier of this provider.
+	 *
+	 * @return string Containing only [a-zA-Z0-9]
+	 */
+	public function identifier() {
+		return 'ocGroupFederatedSharing';
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getShareById($id, $recipientId = null) {
+		if (!ctype_digit($id)) {
+			// share id is defined as a field of type integer
+			// if someone calls the API asking for a share id like "abc" or "42.1"
+			// then there is no point trying to query the database,
+			// and, depending on the database, the query may throw an exception
+			// with a message like "invalid input syntax for type integer"
+			// So throw ShareNotFound now.
+			throw new ShareNotFound();
+		}
+		$qb = $this->dbConnection->getQueryBuilder();
+
+		$qb->select('*')
+			->from($this->shareTable)
+			->where($qb->expr()->eq('id', $qb->createNamedParameter($id)))
+			->andWhere(
+				$qb->expr()->orX(
+					$qb->expr()->eq('share_type', $qb->createNamedParameter(\OCP\Share::SHARE_TYPE_GROUP)),
+					$qb->expr()->eq('share_type', $qb->createNamedParameter(\OCP\Share::SHARE_TYPE_REMOTE_GROUP))
+				)
+			);
+
+		$cursor = $qb->execute();
+		$data = $cursor->fetch();
+		$cursor->closeCursor();
+
+		if ($data === false) {
+			throw new ShareNotFound();
+		}
+
+		try {
+			$share = $this->createShareObject($data);
+		} catch (InvalidShare $e) {
+			throw new ShareNotFound();
+		}
+
+		return $share;
+	}
+
+}
