440ba930cc3cf2fb707a6528839ee7410a499b04
Reza Soltani <soltanireza65@gmail.com>
2023-05-11 19:56:43 +0000

    test_createGroup

--- federatedgroups/tests/Controller/ScimControllerTest.php
+++ federatedgroups/tests/Controller/ScimControllerTest.php
@@ -79,6 +79,8 @@ class ScimControllerTest extends TestCase {
 	 */
 	private $request;
 
+
+
 	protected function setUp(): void {
 		parent::setUp();
 		$this->request = $this->createMock(IRequest::class);
@@ -87,125 +89,132 @@ class ScimControllerTest extends TestCase {
 			"federatedGroups",
 			$this->request,
 			$this->groupManager,
-			// $this->l = $this->createMock(IL10N::class);
-			// $this->logger = $this->createMock(ILogger::class);
-			// $federatedGroupsApp = $this->getMockBuilder(\OCA\FederatedGroups\AppInfo\Application::class);
-			// $this->mixedGroupShareProvider = $federatedGroupsApp->getMixedGroupShareProvider();
-			// $this->dbConnection = \OC::$server->getDatabaseConnection();
-			// $this->userManager = $this->createMock(IUserManager::class);
-			// $this->rootFolder = $this->createMock(IRootFolder::class);
-			// $this->groupNotifications = $this->createMock(GroupNotifications::class);
-			// $this->tokenHandler = $this->createMock(TokenHandler::class);
-			// $this->addressHandler = $this->createMock(AddressHandler::class);
+		);
+
+		$this->dbConnection = \OC::$server->getDatabaseConnection();
+		$this->userManager = $this->createMock(IUserManager::class);
+		$this->groupManager = $this->createMock(IGroupManager::class);
+		$this->rootFolder = $this->createMock(IRootFolder::class);
+		$this->groupNotifications = $this->createMock(GroupNotifications::class);
+		$this->tokenHandler = $this->createMock(TokenHandler::class);
+		$this->tokenHandler->method("generateToken")->willReturn("someToken");
+		$this->addressHandler = $this->createMock(AddressHandler::class);
+		$this->l = $this->createMock(IL10N::class);
+		$this->logger = $this->createMock(ILogger::class);
+
+		$this->mixedGroupShareProvider = new MixedGroupShareProvider(
+			$this->dbConnection,
+			$this->userManager,
+			$this->groupManager,
+			$this->rootFolder,
+			$this->groupNotifications,
+			$this->tokenHandler,
+			$this->addressHandler,
+			$this->l,
+			$this->logger
 		);
 	}
 
 	public function tearDown(): void {
-		// $this->dbConnection->getQueryBuilder()->delete('share')->execute();
-		// $this->dbConnection->getQueryBuilder()->delete('filecache')->execute();
-
 		parent::tearDown();
 	}
 
+	private function checkNeedToSend($newUser, $existingUsers) {
+		$newUserParts = explode("#", $newUser);
+		if (count($newUserParts) == 1) return false; // local user
 
-	public function testGetGroups() {
-		// $expected = [
-		// 	'totalResults' => 2,
-		// 	'Resources' => [
-		// 		[
-		// 			'id' => 'admin',
-		// 			'displayName' => 'admin',
-		// 			'members' => [
-		// 				'value' => 'admin_user',
-		// 				'ref' => '',
-		// 				'displayName' => '',
-		// 			]
-		// 		],
-		// 		[
-		// 			'id' => 'federalists',
-		// 			'displayName' => 'federalists',
-		// 			'members' => [
-		// 				'value' => 'federalist_user',
-		// 				'ref' => '',
-		// 				'displayName' => '',
-		// 			]
-		// 		]
-		// 	]
-		// ];
-		// json_encode($expected);
-
-		[$adminGroupBackendMock, $federalistsGroupBackendMock] = $this->getGroupBackendMocks();
-		[$groupMockAdmin, $groupMockFederalists] = $this->getGroupMocks();
-
-
-		// $adminGroupBackendMock = $this->createMock(\OCP\GroupInterface::class)->expects(self::once())
-		// 	->method("getGroups")->willReturn(["admin"])
-		// 	->method("usersInGroup")->with("admin")->willReturn(["admin_user"]);
-
-		// $federalistsGroupBackendMock = $this->createMock(\OCP\GroupInterface::class)->expects(self::once())
-		// 	->method("getGroups")->willReturn(["federalists"])
-		// 	->method("usersInGroup")->with("federalists")->willReturn(["federalist_user"]);
-
-
-		// $groupMockAdmin = $this->createMock(\OCP\IGroup::class)
-		// 	->method("getGID")->willReturn("admin")
-		// 	->method("getDisplayName")->willReturn("admin")
-		// 	->method("getBackend")->willReturn($adminGroupBackendMock);
-
-		// $groupMockFederalists = $this->createMock(\OCP\IGroup::class)
-		// 	->method("getGID")->willReturn("federalists")
-		// 	->method("getDisplayName")->willReturn("federalists")
-		// 	->method("getBackend")->willReturn($federalistsGroupBackendMock);
-
-
-
-		// $groupBackend = $this->createMock(Backend::class)->expects(self::once())
-		// 	->method("usersInGroup")->willReturn(
-		// 		[
-		// 			"user1#".self::GROUP_NAME."@host1.co",
-		// 			"user2#".self::GROUP_NAME."@host2.co",
-		// 			"local_user"
-		// 		]
-		// 	);
-
-
-		// $this->groupManager->expects($this->once())->method("getBackends")->willReturn([
-		// 	[$adminGroupBackendMock, $federalistsGroupBackendMock]
-		// ]);
-
-
-
-
-		// $this->groupManager->expects($this->exactly(2))->method("get");
-
-
-		$groups = $this->controller->getGroups();
-		
+		if (count($newUserParts) == 2) { // remote user
+			if (str_contains($newUserParts[1], '#') && !str_contains($newUserParts[1], getOurDomain())) {
+				return false;
+			}
+			$newDomain = $newUserParts[1];
+			foreach ($existingUsers as $existingUser) {
+				$existingUserParts = explode("#", $existingUser);
+				if (count($existingUserParts) == 2) {
+					if ($existingUserParts[1] == $newDomain) {
+						return false;
+					}
+				}
+			}
+			return $newDomain;
+		}
+		return false;
 	}
+	public function test_createGroup() {
+		$body = [
+			"id" => "test_group",
+			"members" => [
+				[
+					"value" => "fed_user_2@oc2.docker",
+					"ref" => "",
+					"displayName" => ""
+				]
+			]
+		];
 
-	protected function getGroupBackendMocks() {
-		$adminGroupBackendMock = $this->createMock(\OCP\GroupInterface::class)->expects(self::once())
-			->method("getGroups")->willReturn(["admin"])
-			->method("usersInGroup")->with("admin")->willReturn(["admin_user"]);
+		$groupId = $body["id"];
+		$currentMembers = ["currentMember"];
+		$newMembers = [];
+		$groupMock = $this->createMock(\OCP\IGroup::class);
 
-		$federalistsGroupBackendMock = $this->createMock(\OCP\GroupInterface::class)->expects(self::once())
-			->method("getGroups")->willReturn(["federalists"])
-			->method("usersInGroup")->with("federalists")->willReturn(["federalist_user"]);
+		$groupBackend = $this->createMock(\OC\Group\Backend::class);
 
-		return [$adminGroupBackendMock, $federalistsGroupBackendMock];
-	}
-	protected function getGroupMocks() {
-		[$adminGroupBackendMock, $federalistsGroupBackendMock] = $this->getGroupBackendMocks();
-		$groupMockAdmin = $this->createMock(\OCP\IGroup::class)
-			->method("getGID")->willReturn("admin")
-			->method("getDisplayName")->willReturn("admin")
-			->method("getBackend")->willReturn($adminGroupBackendMock);
+		$this->groupManager->expects($this->once())->method("createGroup")->with($groupId);
 
-		$groupMockFederalists = $this->createMock(\OCP\IGroup::class)
-			->method("getGID")->willReturn("federalists")
-			->method("getDisplayName")->willReturn("federalists")
-			->method("getBackend")->willReturn($federalistsGroupBackendMock);
+		$this->groupManager->expects($this->once())->method("get")->with($groupId)->willReturn($groupMock);
 
-		return [$groupMockAdmin, $groupMockFederalists];
+		$groupMock->expects($this->once())->method('getBackend')->willReturn($groupBackend);
+
+		$groupBackend->expects($this->once())->method('usersInGroup')->with($groupId)->willReturn($currentMembers);
+
+		foreach ($body["members"] as $member) {
+			$userIdParts = explode("@", $member["value"]); // "test_u@pondersource.net"  => ["test_u", "pondersource.net"] 
+			if (count($userIdParts) == 3) {
+				$userIdParts = [$userIdParts[0] . "@" . $userIdParts[1], $userIdParts[2]];
+			}
+			if (count($userIdParts) != 2) {
+				throw new \Exception("cannot parse OCM user " . $member["value"]);
+			}
+			$newMember = $userIdParts[0];
+			if ($userIdParts[1] !== getOurDomain()) {
+				$newMember .= "#" . $userIdParts[1];
+			}
+			if ($userIdParts[1] === IGNORE_DOMAIN) {
+				continue;
+			}
+			$newMembers[] = $newMember;
+		}
+
+		for ($i = 0; $i < count($currentMembers); $i++) {
+			if (!in_array($currentMembers[$i], $newMembers)) {
+				$groupBackend->expects($this->once())->method('removeFromGroup');
+			}
+		}
+		for ($i = 0; $i < count($newMembers); $i++) {
+			if (!in_array($newMembers[$i], $currentMembers)) {
+				$newDomain = $this->checkNeedToSend($newMembers[$i], $currentMembers);
+				if ($newDomain !== false) {
+					try {
+						$this->mixedGroupShareProvider->expects($this->once())->method('sendOcmInviteForExistingShares')->with($newDomain, $groupId);
+					} catch (\Throwable $th) {
+						throw $th;
+					}
+				}
+				$groupBackend->expects($this->once())->method('addToGroup')->with($newMembers[$i], $groupId);
+			}
+		}
+
+		$expected = [
+			"id" => "test_group",
+			"members" => [
+				[
+					"value" => "fed_user_2@oc2.docker",
+					"ref" => "",
+					"displayName" => ""
+				]
+			]
+		];
+
+		$this->assertEquals($body, $expected);
 	}
 }
