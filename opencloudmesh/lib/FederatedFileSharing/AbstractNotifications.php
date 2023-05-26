b2bb556d71533e2a8b8475d495af7f30dbc17a87
Yashar PourMohammad <yasharpm@gmail.com>
2023-05-26 11:16:40 +0000

    Applied license everywhere in code

--- opencloudmesh/lib/FederatedFileSharing/AbstractNotifications.php
+++ opencloudmesh/lib/FederatedFileSharing/AbstractNotifications.php
@@ -5,21 +5,24 @@
  * @author Lukas Reschke <lukas@statuscode.ch>
  * @author Thomas Müller <thomas.mueller@tmit.eu>
  * @author Vincent Petry <pvince81@owncloud.com>
+ * @author Yashar PM <yashar@pondersource.com>
+ * @author Michiel de Jong <michiel@pondersource.com>
  *
- * @copyright Copyright (c) 2018, ownCloud GmbH
- * @license AGPL-3.0
+ * @copyright Copyright (c) 2022, SURF
+ * @license AGPL-3.0-or-later
  *
- * This code is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License, version 3,
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 
